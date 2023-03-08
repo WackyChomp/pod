675a25603e3cff2f7579e3564a60f0b1dafb7668
Wilson Chan <wilsonchan54@gmail.com>
2023-03-07 22:43:04 -0500

    Created PieChart component / created home page with placeholder pie chart

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import { Box, Typography, Stack } from '@pankod/refine-mui';
+import { PieChartProps } from 'interfaces/home'
 
-const PieChart = () => {
+const PieChart = ({ title, value, series, colors }: PieChartProps ) => {
   return (
-    <div>PieChart</div>
+    <Box>
+      <Stack direction='column'>
+        <Typography fontSize={15} color='blue'>
+          {title}
+        </Typography>
+        <Typography fontSize={25} color='magenta'>
+          {value}
+        </Typography>
+      </Stack>
+
+      <ReactApexChart 
+        options={{
+          chart: { type:'donut'},
+          colors,
+          legend: { show: false },
+          dataLabels: { enabled: false }
+        }}
+        series={series}
+        type='donut'
+        width='120px'
+      />
+
+    </Box>
   )
 }
 
